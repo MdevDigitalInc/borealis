333932f41e30f61106f7ae76bcc94d4e55c447da
Ace <ace@mdev.digital>
2018-10-28 15:36:50 -0400

    init

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  section.container
+    logo
+    h1
+      |BOREALIS
+    h2
+      |MDEV NUXT BOILERPLATE
+</template>
+
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss">
+  .container {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
