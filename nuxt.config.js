72520eef8615b183cd96b4401078376b56e5855f
Lucas Moreira <lucas@moreiradevelopment.io>
2018-12-04 13:24:35 -0500

    Added social graphics + documentation

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,26 +18,90 @@ module.exports = {
       { property:'og:type', content: 'website' },
       { property:'og:url', content: '//URL_TO_SITE_TODO' },
       { property:'og:title', content: 'Page Title TODO' },
-      { property:'og:image', content: 'http://moreiradevelopment.io/social/moreira-development-twitcard.png' },
+      { property:'og:image', content: '/social/facebook-og.jpg' },
       // Twitter Card
       { name:'twitter:card', content: 'summary_large_image' },
       { name:'twitter:site', content: '//URL_TO_SITE_TODO' },
       { name:'twitter:title', content: 'Page Title TODO' },
       { name:'twitter:description', content: 'Client Description TODO' },
-      { name:'twitter:image', content: 'http://moreiradevelopment.io/social/moreira-development-twitcard.png' },
+      { name:'twitter:image', content: '/social/twitter-card.png' },
       // Favicon Themeing
       { name:'theme-color', content: '#fff' },
       { name:'msapplication-TileColor', content: '#fff' },
       { name:'msapplication-config', content: '/icons/browserconfig.xml' }
     ],
     link: [
+      // Favicon assets generated through https://realfavicongenerator.net/
+      // Please see README in /static/icons for  more info
       { rel: 'apple-touch-icon', size: '180x180', href: '/icons/apple-touch-icon.png' },
       { rel: 'icon', size: '32x32', type: 'image/png', href: '/icons/favicon-32x32.png' },
       { rel: 'icon', size: '16x16', type: 'image/png', href: '/icons/favicon-16x16.png' },
       { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#fff' },
       { rel: 'shortcut icon', href: '/icons/favicon.ico' },
       { rel: 'manifest', href: '/icons/site.webmanifest' }
-      // Add Google fonts or Font Awesome here.
+      // Typekit
+      { rel: 'stylesheet', href: 'https://use.typekit.net/ubz4jze.css' },
+      // Google Fonts
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans' },
+      // Font-Awesome
+      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.5.0/css/all.css', integrity: 'sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU', crossorigin: 'anonymous' },
+
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#16b1a9' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    '@/assets/styles/global-main.scss',
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://github.com/nuxt-community/axios-module#usage
+    '@nuxtjs/axios'
+  ],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
     ]
   },
 
@@ -58,6 +122,7 @@ module.exports = {
   */
   plugins: [
     // Google Analytics plugin -- NEEDS TO BE CONFIGURED PER CLIENT - TODO
+    // Edit the file in plugins/g-analytics.js
     { src: '~plugins/g-analytics.js', ssr: false },
   ],
 
---
+++ static/icons/README.md
@@ -0,0 +1,12 @@
+# FAVICONS
+Favicons have to be generated for each and every client. Provisions have already
+been made to make sure the assets are served in the head file but you will have
+to replace the assets.
+
+Doing so is quite simple:
+
+1. Go to (Real Favicon Generator)[https://realfavicongenerator.net/] and upload
+   your 500x500 transparent PNG image supplied by the design team
+2. Collect the generated zip file and replace the images inside of
+   '/static/icons'
+3. Test your work by running the build then the server
